063ad9b1a7923b9018a1886e93ff08fd4e985592
Kenneth Soares <kensoares@gmail.com>
2025-06-11 15:40:55 +0200

    saving parsed md

--- script.js
+++ script.js
@@ -4,16 +4,26 @@ import { marked } from "marked"
 window.Alpine = Alpine;
 
 Alpine.data('tabComponent', ()=> ({
+
     tab: 'infos',
+    page_data: {},
     load_file(t) {
         this.tab = t;
         fetch(t + ".md")
             .then(response => response.text())
             .then(markdown => {
-                const htmlContent = marked.parse(markdown);
-                document.getElementById("content").innerHTML = htmlContent;
+                this.page_data[t] = marked.parse(markdown);
+                document.getElementById("content").innerHTML = this.page_data[t];
             })
             .catch(error => console.error("Error loading markdown file:", error));
+    },
+    load_data(t){
+        this.tab = t;
+        if (this.page_data[t] === undefined) {
+            this.load_file(t);
+        } else {
+            document.getElementById("content").innerHTML = this.page_data[t];
+        }
     }
 }));
 
